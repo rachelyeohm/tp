82389e04e50b6a41043958a89b8ba24eecea9ef9
rachelyeohm <rachelyeohm@gmail.com>
2024-10-06 23:58:20 +0800

    Add Remark command

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -12,7 +12,7 @@ public class Remark {
     /**
      * Constructs a {@code Remark}.
      *
-     * @param remark A valid remark. 
+     * @param remark A valid remark.
      */
     public Remark(String remark) {
         requireNonNull(remark);
@@ -26,7 +26,7 @@ public class Remark {
 
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
+        return other == this // short circuit if it is the same object
                 || (other instanceof Remark // instanceof handles nulls
                 && value.equals(((Remark) other).value)); // state check
     }
